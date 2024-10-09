3c0831218750efa88dd77240d7cecf83a745651b
Akira <e.fonquernie@gmail.com>
2024-10-09 12:24:23 +0200

    Ajout Page d'accueil avec barre de navigation

---
+++ incl/header.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Accueil</a></li>
+            <li><a href="../mangas_list.php">Mangas</a></li>
+            <li><a href="../create_review.php">Recommander un manga</a></li>
+            <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
+                <li><a href="../logout.php">Déconnexion</a></li>
+            <?php else : ?>
+                <li><a href="../login.php">Connexion</a></li>
+            <?php endif; ?>
+        </ul> 
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>P M C - Accueil</title>
 </head>
 <body>
+    <?php require_once(__DIR__.'/incl/header.php'); ?>
+
+
+
+
+
+
     
+    <?php require_once(__DIR__.'/incl/footer.php'); ?>
 </body>
 </html>
